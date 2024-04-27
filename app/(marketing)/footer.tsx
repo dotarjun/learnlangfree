0757b79c7804e506ed3777c7de017acc79189b40
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-04-27 16:14:20 +0530

    feat: create marketing page skeleton
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

---
+++ app/(marketing)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <div>
+      <footer className="hidden lg:block h-20 w-full border-b-2 border-slate-200 p-2 ">
+        Footer
+      </footer>
+    </div>
+  );
+};
