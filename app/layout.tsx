507f30c2bd46b8ee26445e36de7f032150629fe2
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-04-27 15:18:36 +0530

    style: update font
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const nunito = Nunito({ subsets: ["latin"] });
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
