cc3435a1a41823ac3ef5bda624708110cf3fe3a4
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-04-30 19:53:49 +0530

    style: update sign up buttons look
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -20,28 +20,30 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn, practice and master new languagues with LibLingo for free
         </h1>
-        <ClerkLoading>
-          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-        </ClerkLoading>
-        <ClerkLoaded>
-          <SignedOut>
-            <SignUpButton mode="modal" forceRedirectUrl={"/learn"}>
-              <Button size={"lg"} variant={"secondary"} className="w-full">
-                Get Started
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal" forceRedirectUrl={"/learn"}>
+                <Button size={"lg"} variant={"secondary"} className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignUpButton mode="modal" forceRedirectUrl={"/learn"}>
+                <Button size={"lg"} variant={"ghost"} className="w-full">
+                  I already have an account
+                </Button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" asChild className="w-full">
+                <Link href="/learn">Continue Learning</Link>
               </Button>
-            </SignUpButton>
-            <SignUpButton mode="modal" forceRedirectUrl={"/learn"}>
-              <Button size={"lg"} variant={"ghost"} className="w-full">
-                I already have an account
-              </Button>
-            </SignUpButton>
-          </SignedOut>
-          <SignedIn>
-            <Button size="lg" variant="secondary" asChild className="w-full">
-              <Link href="/learn">Continue Learning</Link>
-            </Button>
-          </SignedIn>
-        </ClerkLoaded>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
       </div>
     </div>
   );
