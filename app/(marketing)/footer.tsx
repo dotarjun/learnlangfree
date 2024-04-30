51d3036f6090e26551a123013d6797ad47ea1238
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-04-30 20:01:27 +0530

    feat: create footer
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,8 +1,62 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <div>
-      <footer className="hidden lg:block h-20 w-full border-b-2 border-slate-200 p-2 ">
-        Footer
+      <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2 ">
+        <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src="/hr.svg"
+              alt="Croatian"
+              width={40}
+              height={32}
+              className="mr-4 rounded-md"
+            />
+            Croatian
+          </Button>
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src="/es.svg"
+              alt="Spanish"
+              width={40}
+              height={32}
+              className="mr-4 rounded-md"
+            />
+            Spanish
+          </Button>
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src="/fr.svg"
+              alt="French"
+              width={40}
+              height={32}
+              className="mr-4 rounded-md"
+            />
+            French
+          </Button>
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src="/it.svg"
+              alt="Italian"
+              width={40}
+              height={32}
+              className="mr-4 rounded-md"
+            />
+            Italian
+          </Button>
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src="/jp.svg"
+              alt="Japanese"
+              width={40}
+              height={32}
+              className="mr-4 rounded-md"
+            />
+            Japanese
+          </Button>
+        </div>
       </footer>
     </div>
   );
